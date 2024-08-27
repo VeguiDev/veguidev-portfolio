c765972ef812feaef2fa85b42adf6703304d0395
VeguiDev <veguigameplays@gmail.com>
2024-08-27 15:10:39 -0300

    chore: Update npm dependency to latest stable version

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				'blue': "#2980b9",
-				'blue-light': "#3498db"
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        blue: "#2980b9",
+        "blue-light": "#3498db",
+      },
+    },
+  },
+  plugins: [],
+};
