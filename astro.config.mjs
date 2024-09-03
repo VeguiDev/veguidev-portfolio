5958b7eac0ab16cf2651cd4ea0d84ef57a25c48f
VeguiDev <veguigameplays@gmail.com>
2024-09-03 17:52:10 -0300

    Actualizar configuración de astro-i18next para agregar soporte de internacionalización en español

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import astroI18next from "astro-i18next";
 
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), astroI18next()],
   build: {
     assetsPrefix: "./",
   },
   output: "hybrid",
   adapter: vercel(),
-  i18n: {
-    locales: ["en", "es"],
-    defaultLocale: "es",
-  },
 });
